4f086d653341ce74155baa31a5d618d917ce198b
kamerhd <kamerhd@rose-hulman.edu>
2026-01-28 17:19:46 -0500

    variables and methods initialized, and TODOs added

--- src/Position.java
+++ src/Position.java
@@ -4,7 +4,7 @@ public class Position {
     public int row;
     public int col;
 
-    //TODO: complete method translate(mobile.Direction d)
+    //TODO: complete method translate(Direction d)
     public Position translate(Direction d){
 
     }
