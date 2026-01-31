85a0dd5e557752433f28f4525f4fa1f2ebe54724
RHIT-Vermilya <vermildj@rose-hulman.edu>
2026-01-31 18:21:42 -0500

    Update InputHandler.java

--- src/mobile/InputHandler.java
+++ src/mobile/InputHandler.java
@@ -1,20 +1,48 @@
 package mobile;
 
-public class InputHandler {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-    //TODO: complete method getMoveDirection()
-    public Direction getMoveDirection(){
+public class InputHandler implements KeyListener {
 
+    private boolean up, down, left, right, push, pause;
+
+    public Direction getMoveDirection() {
+        if (up) return Direction.UP;
+        if (down) return Direction.DOWN;
+        if (left) return Direction.LEFT;
+        if (right) return Direction.RIGHT;
+        return null;
     }
 
-    //TODO: complete method isPushPressed()
-    public boolean isPushPressed(){
-
+    public boolean isPushPressed() {
+        return push;
     }
 
-    //TODO: complete method isPausePressed()
-    public boolean isPausePressed(){
-
+    public boolean isPausePressed() {
+        return pause;
     }
 
+    public void keyPressed(KeyEvent e) {
+        int k = e.getKeyCode();
+        if (k == KeyEvent.VK_W || k == KeyEvent.VK_UP) up = true;
+        if (k == KeyEvent.VK_S || k == KeyEvent.VK_DOWN) down = true;
+        if (k == KeyEvent.VK_A || k == KeyEvent.VK_LEFT) left = true;
+        if (k == KeyEvent.VK_D || k == KeyEvent.VK_RIGHT) right = true;
+        if (k == KeyEvent.VK_SPACE) push = true;
+        if (k == KeyEvent.VK_P) pause = true;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int k = e.getKeyCode();
+        if (k == KeyEvent.VK_W || k == KeyEvent.VK_UP) up = false;
+        if (k == KeyEvent.VK_S || k == KeyEvent.VK_DOWN) down = false;
+        if (k == KeyEvent.VK_A || k == KeyEvent.VK_LEFT) left = false;
+        if (k == KeyEvent.VK_D || k == KeyEvent.VK_RIGHT) right = false;
+        if (k == KeyEvent.VK_SPACE) push = false;
+        if (k == KeyEvent.VK_P) pause = false;
+    }
+
+    public void keyTyped(KeyEvent e) {}
 }
+
