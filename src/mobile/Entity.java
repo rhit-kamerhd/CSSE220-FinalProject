a1e0e8ea0f5d7b9048a34603aa4636ef6d8dbcf0
RHIT-Vermilya <vermildj@rose-hulman.edu>
2026-02-03 20:47:01 -0500

    javadoc.java

--- src/mobile/Entity.java
+++ src/mobile/Entity.java
@@ -3,17 +3,35 @@ package mobile;
 import game.GameWorld;
 import game.Position;
 
+/**
+ * Base class for all movable objects in the game world.
+ * Stores position and defines the update behavior contract.
+ */
 public abstract class Entity {
 
+    /** Current position of the entity in the world grid */
     private Position pos;
 
+    /**
+     * Gets the entity's current position.
+     * @return current Position
+     */
     public Position getPosition() {
         return pos;
     }
 
+    /**
+     * Sets the entity's position.
+     * @param p new Position
+     */
     public void setPosition(Position p) {
         pos = p;
     }
 
+    /**
+     * Updates the entity each game tick.
+     * @param world the game world the entity exists in
+     */
     public abstract void update(GameWorld world);
 }
+
