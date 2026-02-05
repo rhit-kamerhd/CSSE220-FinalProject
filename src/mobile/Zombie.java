9a01bae666720b8696f3079116ada45470811275
RHIT-Vermilya <vermildj@rose-hulman.edu>
2026-02-05 11:37:03 -0500

    Update Zombie.java

--- src/mobile/Zombie.java
+++ src/mobile/Zombie.java
@@ -2,31 +2,31 @@ package mobile;
 
 import java.util.Random;
 import game.GameWorld;
+import game.HUD;
 import game.Position;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Represents an enemy zombie that moves randomly.
  */
 public class Zombie extends Entity {
 
-    private final Random rand = new Random();
-    private Image sprite;
-
+    private final Random rand = new Random();;
+    public final BufferedImage sprite;
     /**
      * Creates a zombie.
      * @param p starting position
      */
-    public Zombie(Position p) {
+    public Zombie(Position p) throws IOException{
         setPosition(p);
-        sprite = new ImageIcon(getClass().getResource("/zombie_sprite.png")).getImage();
+        sprite = HUD.load("/zombie_sprite.png");
     }
 
-    /** @return zombie sprite image */
-    public Image getSprite() {
-        return sprite;
-    }
 
     /**
      * Chooses a random valid movement direction.
@@ -58,5 +58,6 @@ public class Zombie extends Entity {
         tryMove(d, world);
     }
 }
+
 
 
